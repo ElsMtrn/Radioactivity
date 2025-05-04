d17dfe708888df65a9508ff5d8ff506ed4e7fb33
ElsMtrn <ellismatern@ellis-mac-os>
2025-05-04 19:13:21 +0200

    Sync the already created files

---
+++ src/main/java/net/elsmtrn/radioactivity/item/RadioactiveSpiderItem.java
@@ -0,0 +1,56 @@
+
+package net.elsmtrn.radioactivity.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.UseAction;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+import net.minecraft.client.util.ITooltipFlag;
+
+import net.elsmtrn.radioactivity.RadioactivityModElements;
+
+import java.util.List;
+
+@RadioactivityModElements.ModElement.Tag
+public class RadioactiveSpiderItem extends RadioactivityModElements.ModElement {
+	@ObjectHolder("radioactivity:radioactive_spider")
+	public static final Item block = null;
+	public RadioactiveSpiderItem(RadioactivityModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new FoodItemCustom());
+	}
+	public static class FoodItemCustom extends Item {
+		public FoodItemCustom() {
+			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(64).rarity(Rarity.EPIC)
+					.food((new Food.Builder()).hunger(4).saturation(0.3f).setAlwaysEdible().build()));
+			setRegistryName("radioactive_spider");
+		}
+
+		@Override
+		public int getUseDuration(ItemStack stack) {
+			return 10;
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack itemstack) {
+			return UseAction.EAT;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("Eat this to get superpowers..."));
+		}
+	}
+}
